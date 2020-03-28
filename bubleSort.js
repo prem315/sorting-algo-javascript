a37c28550bdc337336638382cb8ea514e1e18dca
Prem Patel <prem.patel3105@gmail.com>
2020-03-28 15:34:07 +0530

    added bublesort algo solution

--- bubleSort.js
+++ bubleSort.js
@@ -1,1 +1,14 @@
-console.log("hello");
+function bubleSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  console.log("sorted arr", arr);
+}
+
+bubleSort([5, 4, 10, 1, 6, 2]);
